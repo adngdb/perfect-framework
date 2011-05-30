3c93425134b1f6ae7c9683f3a9ef02aa9bbe1ca0
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-29 23:11:49 -0700

    Adding a default configuration class.

---
+++ core/config.php
@@ -0,0 +1,30 @@
+<?php
+
+class Option
+{
+    public $doc = '';
+    public $value = null;
+}
+
+class Config
+{
+    public function __construct()
+    {
+        $this->version = new Option();
+        $this->version->doc = 'Current version of Perfect Framework';
+        $this->version->value = '0.1';
+
+        $this->layout = new Option();
+        $this->layout->doc = 'Basic template to render';
+        $this->layout->value = '_layout';
+    }
+
+    public function get($option, $defaultValue = null)
+    {
+        if ($this->$option !== null)
+        {
+            return $this->$option->value;
+        }
+        return $defaultValue;
+    }
+}
