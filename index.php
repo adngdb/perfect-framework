2ab40b532137799916ec85589f5817fb9852f7c4
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-30 00:48:03 -0700

    Define APPLICATION_ROOT and use it.

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 define('PERFECT_VERSION', '0.1');
 
-require_once('core/perfect.php');
+define('APPLICATION_ROOT', realpath( dirname( __FILE__ ) ) . '/');
+
+require_once(APPLICATION_ROOT.'core/perfect.php');
 
 Perfect::init();
 Perfect::run();
