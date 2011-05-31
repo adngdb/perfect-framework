c34d0ac06fcc140c1f2156ebb01bbc2cf7234ed6
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-30 22:00:21 -0700

    Better parsing of the query, instanciates and calls the controller.

--- core/perfect.php
+++ core/perfect.php
@@ -6,6 +6,7 @@ require_once(APPLICATION_ROOT.'core/controller.php');
 class Perfect
 {
     public static $config = null;
+    public static $controller = null;
 
     public static function init()
     {
@@ -20,6 +21,10 @@ class Perfect
         var_dump($controller);
         var_dump($method);
         var_dump($params);
+
+        // Instanciate the controller, execute the method
+        self::$controller = new $controller();
+        call_user_func_array(array(self::$controller, $method), $params);
     }
 
     public static function render()
@@ -39,35 +44,45 @@ class Perfect
         $method = '';
         $params = array();
 
-        $params = $_GET;
-        unset($params['perfect']);
-
-        $query = $_GET['perfect'];
-        $parts = explode('/', $query);
-
-        $size = count($parts);
-
-        switch ($size)
+        if (!isset($_GET['perfect']) || empty($_GET['perfect']))
         {
-            case 0:
-                $controller = self::$config->get('defaultController', 'home');
-                $method = self::$config->get('defaultMethod', 'index');
-                break;
-            case 1:
-                $controller = $parts[0];
-                $method = self::$config->get('defaultMethod', 'index');
-                break;
-            case 2:
-                $controller = $parts[0];
-                $method = $parts[1];
-                break;
-            default:
-                $controller = $parts[0];
-                $method = $parts[1];
-                unset($parts[0]);
-                unset($parts[1]);
-                $params['perfect'] = $parts;
+            $controller = self::$config->get('defaultController', 'home');
+            $method = self::$config->get('defaultMethod', 'index');
         }
+        else
+        {
+            $query = $_GET['perfect'];
+            $parts = explode('/', $query);
+
+            $size = count($parts);
+
+            switch ($size)
+            {
+                case 1:
+                    $controller = $parts[0];
+                    $method = self::$config->get('defaultMethod', 'index');
+                    break;
+                case 2:
+                    $controller = $parts[0];
+                    $method = $parts[1];
+                    break;
+                default:
+                    $controller = $parts[0];
+                    $method = $parts[1];
+                    unset($parts[0]);
+                    unset($parts[1]);
+                    $params['perfect'] = $parts;
+            }
+        }
+
+        // Turn the controller name into a class name
+        $words = explode('-', $controller);
+        $controller = '';
+        foreach ($words as $word)
+        {
+            $controller .= ucfirst($word);
+        }
+        $controller .= 'Controller';
 
         return array($controller, $method, $params);
     }
