9fd7b4c48672ea88379a1de1fae34bb24a94877d
Adrian Gaudebert <adrian@gaudebert.fr>
2011-06-01 23:24:59 -0700

    Testing new features.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,6 +9,9 @@ class HomeController extends Controller
     public function index()
     {
         echo 'hehehe';
+        $tt = 34;
+
+        return get_defined_vars();
     }
 
     public function name($name, $age)
